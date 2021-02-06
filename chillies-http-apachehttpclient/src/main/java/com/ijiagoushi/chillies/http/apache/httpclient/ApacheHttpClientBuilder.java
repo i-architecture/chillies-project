4c340fc3470173c08a17bf40b4a5b35c12329e02
miles.tang <and.mz.yq@gmail.com>
2021-02-07 02:38:44 +0800

    新增了chillies-http-apachehttpclient模块：支持了Apache HttpClient的底层引擎

---
+++ chillies-http-apachehttpclient/src/main/java/com/ijiagoushi/chillies/http/apache/httpclient/ApacheHttpClientBuilder.java
@@ -0,0 +1,15 @@
+package com.ijiagoushi.chillies.http.apache.httpclient;
+
+import com.ijiagoushi.chillies.http.HttpClient;
+import com.ijiagoushi.chillies.http.HttpClientBuilder;
+import com.ijiagoushi.chillies.http.HttpOptions;
+import org.jetbrains.annotations.Nullable;
+
+public class ApacheHttpClientBuilder extends HttpClientBuilder {
+
+    @Override
+    public HttpClient build(@Nullable HttpOptions options) {
+        return new ApacheHttpClient(options);
+    }
+
+}
